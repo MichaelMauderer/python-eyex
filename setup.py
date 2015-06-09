874b390af27da8869816ee5f56ef131844a824ea
Michael Mauderer <mail@michaelmauderer.com>
2015-06-09 10:38:10 +0100

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-eyex',
+      version='0.1',
+      description='Simple wrapper for EyeX SDK',
+      author='Michael Mauderer',
+      author_email='mail@michaelmauderer.com',
+      packages=['eyex'],
+     )
