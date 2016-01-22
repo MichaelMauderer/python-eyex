6cac38bd80e3134d03f5222a166e5ab704e802b7
Michael Mauderer <mail@michaelmauderer.com>
2016-01-22 16:27:12 +0000

    Bumped version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-eyex',
-      version='0.1',
+      version='0.2',
       description='Simple wrapper for EyeX SDK',
       author='Michael Mauderer',
       author_email='mail@michaelmauderer.com',
